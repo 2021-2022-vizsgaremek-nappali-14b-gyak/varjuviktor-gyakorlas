3b0f9d79cc6e64bfc76d554784315b9b1044a19a
Viktor Crow <varjuviktor69@gmail.com>
2022-01-05 14:05:59 +0100

    .

---
+++ Desktop/src/Vizsgaremek/Models/ProgramInfo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+namespace Vizsgaremek.Models
+{
+    public class ProgramInfo
+    {
+        private Version version;
+        private string authors;
+        private string title;
+        private string description;
+        private string company;
+
+        public ProgramInfo()
+        {
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+
+            foreach (Attribute attr in Attribute.GetCustomAttributes(assembly))
+            {
+                if (attr.GetType() == typeof(AssemblyTitleAttribute))
+                    title = ((AssemblyTitleAttribute)attr).Title;
+                else if (attr.GetType() == typeof(AssemblyDescriptionAttribute))
+                    description = ((AssemblyDescriptionAttribute)attr).Description;
+                else if (attr.GetType() == typeof(AssemblyCompanyAttribute))
+                    company = ((AssemblyCompanyAttribute)attr).Company;
+
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var assemblyVerzio = assembly.GetName().Version;
+                return assemblyVerzio;
+            }
+        }
+
+        public string Authors
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public string Company { get => company; set => company = value; }
+
+        
+
+
+
+
+    }
+}
