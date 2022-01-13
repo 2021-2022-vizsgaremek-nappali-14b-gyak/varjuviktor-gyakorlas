8d35e722274592e3c5640dadee67ae1fcea0749f
Viktor Crow <varjuviktor69@gmail.com>
2022-01-13 08:30:54 +0100

    db source osztály a modellben

--- Desktop/src/Vizsgaremek/Models/DataBaseSource.cs
+++ Desktop/src/Vizsgaremek/Models/DataBaseSource.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Vizsgaremek.Models
 {
+    public enum DBSOurce { NONE, LOCALHOST, DEVOPS}
     class DataBaseSource
     {
+        DBSOurce dBSOurce;
+
+        public DBSOurce DBSOurce { get => dBSOurce; set => dBSOurce = value; }
+
+        public DataBaseSource(DBSOurce dBSOurce)
+        {
+            DBSOurce = DBSOurce.NONE;
+           
+        }
     }
 }
